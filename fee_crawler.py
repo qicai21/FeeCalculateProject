610d820e6bd6597d8c4b40f3b6d46ebc0397abaf
Handymax <qicai21@gmail.com>
2022-01-26 00:58:03 +0800

    FeeCrawler爬虫部分功能基本齐备,通过了测试

---
+++ fee_crawler.py
@@ -0,0 +1,232 @@
+import requests
+import json
+import random
+import time
+
+
+class FeeCrawler():
+    def __init__(self, delay=0):
+        self.delay = delay
+        self.headers = None
+        self.query_code = None
+        self.cookie = None
+        self.token = None
+        self.refresh_query_code_and_cookie()
+
+        # for post data:
+        self.start_lj_data = None
+        self.start_lj_code = None
+        self.start_station_name = None
+        self.start_station_code = None
+        self.start_station_data = None
+
+        self.end_lj_data = None
+        self.end_lj_code = None
+        self.end_station_name = None
+        self.end_station_code = None
+        self.end_station_data = None
+
+        self.cargo_name = None
+        self.cargo_data = None
+        self.cargo_code = None
+
+        self.start_station_load_fee = '0'
+        self.end_station_discharge_fee = '0'
+
+    def refresh_query_code_and_cookie(self):
+        url = 'https://ec.95306.cn/api/gxzx/verfication/query'
+        resp = requests.post(url, data={})
+        if resp.status_code == 200:
+            session = resp.headers['Set-Cookie'].split(';')[0]
+            self.cookie = f'95306-1.6.10-loginType=outer;{session}'
+            self.query_code = resp.json()['data']['randomCode']
+            self.token = resp.json()['data']['code']
+            if self.delay > 0:
+                time.sleep(self.delay)
+                print(f'refresh successful, Code:{self.query_code}')
+        else:
+            print(f'refresh code&cookie failed, status_code:{resp.status_code}')
+
+    def send_cargo_name_request(self, name):
+        data = {'q': name, 'limit': "50", 'isShieldYsfs': 'true'}
+        length = len(json.dumps(data, ensure_ascii=False)) + 1
+        url = 'https://ec.95306.cn/api/zd/pm/queryList'
+        headers = create_headers_by(length, self.cookie)
+        resp = requests.post(url, headers=headers, json=data)
+        return resp.json()
+
+    def send_station_request(self, name):
+        data = {'q': name, 'ljdm': '', 'limit': '50'}
+        length = len(json.dumps(data, ensure_ascii=False)) + 1
+        url = 'https://ec.95306.cn/api/zd/lj/queryTmismForYfcx'
+        headers = create_headers_by(length, self.cookie)
+        resp = requests.post(url, headers=headers, json=data)
+        return resp.json()
+
+    def query_station_by_name(self, name):
+        stations = self.send_station_request(name)
+        if len(stations['data']) == 0:
+            new_name = input('未查到任何站名,请输入新站名:')
+            return self.query_station_by_name(new_name)
+        elif len(stations['data']) == 1:
+            name = stations['data'][0]['hzzm']
+            print('查到车站:' + name)
+            return stations['data'][0]
+        else:
+            names = [i['hzzm'] for i in stations['data']]
+            names_txt = ', '.join([f'{i}: {n};' for i, n in zip(range(len(names)), names)])
+            select = input('查到多条结果:' + names_txt + '输入序号选定车站,输入站名文字重新搜索')
+            try:
+                index = int(select)
+                return stations['data'][index]
+            except ValueError:
+                new_name = select
+                return self.query_station_by_name(new_name)
+
+    def send_lj_request(self, lj_name):
+        data = {'q': lj_name, 'limit': '50'}
+        length = 60  # 不再算了,60足够了
+        url = 'https://ec.95306.cn/api/zd/lj/queryLjs'
+        headers = create_headers_by(length, self.cookie)
+        resp = requests.post(url, headers=headers, json=data)
+        resp_json = resp.json()
+        if resp.status_code != 200 or resp_json['msg'] != 'OK':
+            return None
+        return resp_json['data'][0]
+
+    def set_start_station(self, name):
+        resp_data = self.query_station_by_name(name)
+        self.start_station_code = resp_data['dbm']
+        self.start_station_name = resp_data['hzzm']
+        self.start_station_data = resp_data
+        self.start_lj_code = resp_data['ljm']
+        start_lj_name = resp_data['ljqc']
+        data = self.send_lj_request(start_lj_name)
+        if data:
+            self.start_lj_data = data
+
+    def set_end_station(self, name):
+        resp_data = self.query_station_by_name(name)
+        self.end_station_code = resp_data['dbm']
+        self.end_station_name = resp_data['hzzm']
+        self.end_station_data = resp_data
+        self.end_lj_code = resp_data['ljm']
+        end_lj_name = resp_data['ljqc']
+        data = self.send_lj_request(end_lj_name)
+        if data:
+            self.end_lj_data = data
+
+    def query_cargo_by_name(self, name):
+        cargo = self.send_cargo_name_request(name)
+        if len(cargo['data']) == 0:
+            new_name = input('未查到任何货物名称,请输入货名:')
+            return self.query_cargo_by_name(new_name)
+        elif len(cargo['data']) == 1:
+            name = cargo['data'][0]['pmhz']
+            print('查到货名:' + name)
+            return cargo['data'][0]
+        else:
+            names = [i['pmhz'] for i in cargo['data']]
+            names_txt = ', '.join([f'{i}: {n};' for i, n in zip(range(len(names)), names)])
+            select = input('查到多条结果:' + names_txt + '输入序号选定货名,输入货名文字重新搜索')
+            try:
+                index = int(select)
+                return cargo['data'][index]
+            except ValueError:
+                new_name = select
+                return self.query_cargo_by_name(new_name)
+
+    def set_cargo(self, name):
+        resp_data = self.query_cargo_by_name(name)
+        self.cargo_name = resp_data['pmhz']
+        self.cargo_data = resp_data
+        self.cargo_code = resp_data['dm']
+
+    def get_post_data(self):
+        data = {
+            'flj': self.start_lj_data,
+            'dlj': self.end_lj_data,
+            'hw': '1',
+            'fzhzzm': self.start_station_name,
+            'fzObj': self.start_station_data,
+            'dzhzzm': self.end_station_name,
+            'dzObj': self.end_station_data,
+            'hwmcObj': self.cargo_data,
+            'hwmc': self.cargo_name,
+            'plObj': '',
+            'pl': '',
+            'xplObj': '',
+            'xpl': '',
+            'code': self.query_code,
+            'token': self.token,
+            'dz': self.end_station_code,
+            'dzljm': self.end_lj_code,
+            'dztlzx': self.end_station_discharge_fee,  # 到站装卸
+            'fz': self.start_station_code,
+            'fzljm': self.start_lj_code,
+            'fztlzx': self.start_station_load_fee,  # 发站装卸
+            'hzpm': self.cargo_name,
+            'pm': self.cargo_code,
+            'shsm': '0',  # 送货上门
+            'smqh': '0',  # 上门取货
+            'smxc': '0',  # 上门卸车
+            'smzc': '0',  # 上门装车
+            'fsqhlc': '0',  # 上门取货里程
+            'ddqhlc': '0'  # 送货上门里程
+        }
+        return data
+
+    def query_crt_fee(self, start_station, end_station, cargo, start_stn_load=False, end_stn_discharge=False):
+        if start_stn_load:
+            self.start_station_load_fee = '1'
+        if end_stn_discharge:
+            self.end_station_discharge_fee = '1'
+        self.set_start_station(start_station)
+        self.set_end_station(end_station)
+        self.set_cargo(cargo)
+        data = self.get_post_data()
+        length = len(json.dumps(data, ensure_ascii=False)) + 10
+        headers = create_headers_by(length, self.cookie)
+        url = 'https://ec.95306.cn/api/zx/businessFor/carriageCalculateNew'
+        resp = requests.post(url, headers=headers, json=data)
+        if resp.status_code != 200 or resp.json()['msg'] != 'OK':
+            print(f'查询失败!return code: {resp.status_code}. retrun msg:{resp.json()["msg"]}')
+            return None
+        return resp.json()['data']
+
+
+
+def make_random_cookie():
+    chars = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+             'abcdefghijklmnopqrstuvwxyz'
+             '0123456789')
+    seesion = [random.choice(chars) for i in range(48)]
+    return ''.join(seesion)
+
+
+def create_headers_by(data_length, cookie_str):
+    return {
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+        'channel': 'P',
+        'Connection': 'Keep-Alive',
+        'Content-Length': str(data_length),
+        'Content-Type': 'application/json',
+        'Host': 'ec.95306.cn',
+        'Origin': 'http://ec.95306.cn',
+        'Referer': 'http://ec.95306.cn/del-query',
+        'type': 'outer',
+        'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+
+                       'AppleWebKit/537.36 (KHTML, like Gecko) '
+
+                       'Chrome/96.0.4664.110 '
+
+                       'Safari/537.36'),
+        'Cookie': cookie_str
+    }
+
+
+if __name__ == '__main__':
+    print('ok!')
---
+++ tests.py
@@ -0,0 +1,76 @@
+import unittest
+from fee_crawler import FeeCrawler
+
+
+class CrawlerTest(unittest.TestCase):
+    @unittest.skip('确定成功')
+    def test_crawler_can_refresh_cookie_code_and_token(self):
+        crl = FeeCrawler()
+        cookie = crl.cookie
+        code = crl.query_code
+        token = crl.token
+        crl.refresh_query_code_and_cookie()
+        self.assertNotEqual(cookie, crl.cookie)
+        self.assertNotEqual(code, crl.query_code)
+        self.assertNotEqual(token, crl.token)
+
+    @unittest.skip('确定成功')
+    def test_query_cargo_names(self):
+        crl = FeeCrawler()
+        content = crl.send_cargo_name_request('化肥')
+        self.assertEqual(content['msg'], 'OK')
+
+    @unittest.skip('确定成功')
+    def test_query_station_and_set_station_info(self):
+        crl = FeeCrawler()
+        crl.set_start_station('高桥')
+        self.assertEqual(crl.start_lj_code, '00002')
+        self.assertEqual(crl.start_lj_data['qc'], '沈阳局')
+        self.assertEqual(crl.start_lj_data['dm'], 'T00')
+        self.assertEqual(crl.start_station_name, '高桥镇')
+        self.assertEqual(crl.start_station_data['tmism'], '51632')
+
+    @unittest.skip('确定成功')
+    def test_popup_station_selection_if_queried_multi_record(self):
+        crl = FeeCrawler()
+        station = crl.query_station_by_name('高')
+        self.assertEqual(station['hzzm'], '高桥镇')
+
+    @unittest.skip('确定成功')
+    def test_query_cgo_and_set_cgo_data(self):
+        crl = FeeCrawler()
+        crl.set_cargo('化肥')
+        self.assertEqual(crl.cargo_name, '尿素(化肥)')
+        self.assertEqual(crl.cargo_data['pym'], 'NSH')
+        self.assertEqual(crl.cargo_code, '1310013')
+
+    @unittest.skip('确定成功')
+    def test_query_price_post_data_format_and_code_token_is_correct(self):
+        crl = FeeCrawler()
+        crl.set_cargo('尿素(化肥)')
+        crl.set_start_station('高桥镇')
+        crl.set_end_station('四平')
+        formated_data_keys = [
+            'flj', 'dlj', 'hw', 'fzhzzm', 'fzObj', 'dzhzzm', 'dzObj',
+            'hwmcObj', 'hwmc', 'plObj', 'pl', 'xplObj', 'xpl', 'code',
+            'token', 'dz', 'dzljm', 'dztlzx', 'fz', 'fzljm', 'fztlzx',
+            'hzpm', 'pm', 'shsm', 'smqh', 'smxc', 'smzc', 'fsqhlc', 'ddqhlc'
+        ]
+        data = crl.get_post_data()
+        print(crl.cookie)
+        print(data['code'])
+        print(data['token'])
+        self.assertEqual(set(data.keys()), set(formated_data_keys))
+        self.assertIsNotNone(data['code'])
+        self.assertIsNotNone(data['token'])
+
+    def test_query_crt_fee_successful(self):
+        crl = FeeCrawler()
+        result = crl.query_crt_fee('高桥镇', '四平', '尿素(化肥)')
+        self.assertIsNotNone(result)
+        print(result)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
